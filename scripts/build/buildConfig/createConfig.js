b1a35b9281d780159966b0ba833adf1166a80030
meke <3197801970@qq.com>
2022-09-09 20:07:50 +0800

    build(buildconfig): add setup external option

--- scripts/build/buildConfig/createConfig.js
+++ scripts/build/buildConfig/createConfig.js
@@ -1,9 +1,23 @@
 import { createPlugin } from "./createPlugin";
 export function createConfig(options) {
-  const { input, output, name, format } = options;
+  const { input, output, name, format, prod, packageJson } = options;
+  const { dependencies, peerDependencies } = packageJson;
+
   const plugins = createPlugin(options);
+
+  let external = [];
+
+  if (format === "iife") {
+  } else {
+    external = [
+      ...Object.keys(dependencies || {}),
+      ...Object.keys(peerDependencies || {}),
+    ];
+  }
+
   return {
     input,
+    external,
     plugins: plugins,
     output: {
       file: output,
